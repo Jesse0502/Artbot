49949b017a31200d21055674b0a67fb6285c8d1c
Jesse0502 <jas0502singh@gmail.com>
2022-06-21 21:13:09 +0530

    change 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,20 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { Logo } from "./Logo";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>Hello</VStack>
+        <VStack spacing={8}>
+          <Text>Hello</Text>
+        </VStack>
       </Grid>
     </Box>
   </ChakraProvider>
