f2354365a02c890845bdd82aadd9f8a4a459d691
Jesse0502 <jas0502singh@gmail.com>
2023-02-04 18:09:32 +0530

    added scroll to notifications secion

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -8,7 +8,7 @@ const Notifications = () => {
   return (
     <Box p="5">
       <Heading>Notifications</Heading>
-      <Flex flexDir="column-reverse" mt="10" h="76vh" overflow="auto">
+      <Flex flexDir="column-reverse" mt="10" h="70vh" overflow="auto">
         {notifications.length > 0 ? (
           notifications.map((i: any) => (
             <Flex
